c8958ac4730851f8bba18569b191cce14da36d4d
urospetrov <91796126+urospetrov@users.noreply.github.com>
2022-04-06 20:54:16 +0200

    Initial setup with models.

---
+++ Model/Service.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Service
+    {
+        private int serviceId;
+        private string name;
+        private int price;
+        private int duration; // In minutes.
+        private int pointPrice;
+        private int pointReward;
+        private string category;
+
+        public Service(int serviceId, string name, int price, int duration, int pointPrice, int pointReward, string category)
+        {
+            this.serviceId = serviceId;
+            this.name = name;
+            this.price = price;
+            this.duration = duration;
+            this.pointPrice = pointPrice;
+            this.pointReward = pointReward;
+            this.category = category;
+        }
+
+        public int ServiceId { get => serviceId; set => serviceId = value; }
+        public string Name { get => name; set => name = value; }
+        public int Price { get => price; set => price = value; }
+        public int Duration { get => duration; set => duration = value; }
+        public int PointPrice { get => pointPrice; set => pointPrice = value; }
+        public int PointReward { get => pointReward; set => pointReward = value; }
+        public string Category { get => category; set => category = value; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Service service &&
+                   serviceId == service.serviceId &&
+                   name == service.name &&
+                   price == service.price &&
+                   duration == service.duration &&
+                   pointPrice == service.pointPrice &&
+                   pointReward == service.pointReward &&
+                   category == service.category;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(serviceId, name, price, duration, pointPrice, pointReward, category);
+        }
+    }
+}
---
+++ Model/User.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class User
+    {
+        private int userId;
+        private string firstName;
+        private string lastName;
+        private int phoneNumber;
+        private string email;
+        private string gender;
+        private int points;
+        private int loyaltyCardId;
+
+        public User(int userId, string firstName, string lastName, int phoneNumber, string email, string gender, int points, int loyaltyCardId)
+        {
+            this.userId = userId;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+            this.gender = gender;
+            this.points = points;
+            this.loyaltyCardId = loyaltyCardId;
+        }
+
+        public int UserId { get => userId; set => userId = value; }
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public int PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
+        public string Email { get => email; set => email = value; }
+        public string Gender { get => gender; set => gender = value; }
+        public int Points { get => points; set => points = value; }
+        public int LoyaltyCardId { get => loyaltyCardId; set => loyaltyCardId = value; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is User user &&
+                   userId == user.userId &&
+                   firstName == user.firstName &&
+                   lastName == user.lastName &&
+                   phoneNumber == user.phoneNumber &&
+                   email == user.email &&
+                   gender == user.gender &&
+                   points == user.points &&
+                   loyaltyCardId == user.loyaltyCardId;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(userId, firstName, lastName, phoneNumber, email, gender, points, loyaltyCardId);
+        }
+    }
+}
