8f0319429a5163ad565e7defbb755a860e46b959
My Pc <sasakiticsanz@gmail.com>
2022-04-11 20:49:36 +0200

    DOne

--- TestingCA/UIHandler/TestingUIHandler.cs
+++ TestingCA/UIHandler/TestingUIHandler.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Common.Methods;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,49 +9,40 @@ namespace TestingCA.UIHandler
 {
     public class TestingUIHandler
     {
-        private static readonly AppointmentUIHandler appointmentUI = new AppointmentUIHandler();
-        private static readonly CustomerUIHandler customerUI = new CustomerUIHandler();
-        private static readonly ServiceUIHandler serviceUI = new ServiceUIHandler();
-        private static readonly SIAUIHandler siaUI = new SIAUIHandler();
-        private static readonly WorkerUIHandler workerUI = new WorkerUIHandler();
-
+        private static readonly ServiceUIHandler usluga = new ServiceUIHandler();
+        private static readonly Recognition funkcije = new Recognition();
         public void HandleMainMenu()
         {
             string answer;
             do
             {
                 Console.Clear();
-                Console.WriteLine("\nZa izlazak uneti x\nOdabir tabele za testiranje:");
-                Console.WriteLine("1. Appointment Testing");
-                Console.WriteLine("2. Customer Testing");
-                Console.WriteLine("3. Service Testing");
-                Console.WriteLine("4. SIA (services in a appointment) Testing");
-                Console.WriteLine("5. Worker Testing");
+                Console.WriteLine("\nZa izlazak uneti x\nOpcije:");
+                Console.WriteLine("1. Testiranje usluga");
+                Console.WriteLine("2. Testiraj filtriranje");
 
                 answer = Console.ReadLine();
 
                 switch (answer)
                 {
                     case "1":
-                        appointmentUI.MenuHandler();
+                        usluga.MenuHandler();
                         break;
                     case "2":
-                        customerUI.MenuHandler();
-                        break;
-                    case "3":
-                        serviceUI.MenuHandler();
-                        break;
-                    case "4":
-                        siaUI.MenuHandler();
-                        break;
-                    case "5":
-                        workerUI.MenuHandler();
+                        TestIt();
                         break;
                     case "x":
                         return;
                 }
 
             } while (true);
+        }
+
+        public void TestIt() {
+            Console.WriteLine("Unesite string za filtraciju");
+            string answer = Console.ReadLine();
+            funkcije.FilterString(answer);
+            Console.ReadKey();
         }
 
     }
