01d41af914a26239bb88e07ae9928b65d0f58c66
urospetrov <91796126+urospetrov@users.noreply.github.com>
2022-04-12 21:40:25 +0200

    Customer Add, Alter, Delete methods done.

--- ViewModel/ViewModels/CustomerViewModel.cs
+++ ViewModel/ViewModels/CustomerViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Model.FrontendModel;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,11 +16,126 @@ namespace ViewModel.ViewModels
         private CustomerInfoViewModel customerInfoViewModel = new CustomerInfoViewModel();
         private BindableBase currentCustomerViewModel;
 
+        private CustomerFront selectedItem;
+        private bool canAlter = false;
+        private bool canDelete = false;
+
         public MyICommand<string> NavCommand { get; set; }
+        public MyICommand ItemSelectedCommand { get; set; }
+        public MyICommand AlterCommand { get; set; }
+        public MyICommand DeleteCommand { get; set; }
+        public MyICommand CancelCommand { get; set; }
+
+        public static BindingList<CustomerFront> Customers { get; private set; }
+
         public CustomerViewModel()
         {
+            Customers = new BindingList<CustomerFront>();
+            Customers.Add(new CustomerFront(0, "Marko", "Markovic", "0613228203", "marko123@gmail.com", "Male", 12, "231465231"));
+
             NavCommand = new MyICommand<string>(OnNav);
+            ItemSelectedCommand = new MyICommand(OnSelect);
+            AlterCommand = new MyICommand(OnAlter);
+            DeleteCommand = new MyICommand(OnDelete);
+            CancelCommand = new MyICommand(OnCancel);
+
             CurrentCustomerViewModel = customerFilterViewModel;
+        }
+
+        private void OnCancel()
+        {
+            if(CurrentCustomerViewModel == customerAddViewModel)
+            {
+                customerAddViewModel.ClearInput();
+
+                CustomerFront newCust = SelectedItem;
+                Customers.Remove(SelectedItem);
+                Customers.Add(newCust);
+                CanAlter = false;
+                CanDelete = false;
+
+                // SelectedItem = null
+                // ~~ Problem, SelectedItem remains selected. Bypassed, costly.
+
+                OnNav("filter");
+            }
+            else if(CurrentCustomerViewModel == customerFilterViewModel)
+            {
+                //customerFilterViewModel.ClearInput();
+            }
+            else if(CurrentCustomerViewModel == customerInfoViewModel)
+            {
+                OnNav("filter");
+            }
+        }
+
+        private void OnDelete()
+        {
+            if (SelectedItem == null)
+            {
+                return;
+            }
+            Customers.Remove(SelectedItem);
+            //SelectedItem = null; ~~ Doesn't work
+            CanAlter = false;
+            CanDelete = false;
+            OnNav("filter");
+        }
+
+        private void OnAlter()
+        {
+            if(CurrentCustomerViewModel != customerAddViewModel)
+            {
+                CurrentCustomerViewModel = customerAddViewModel;
+
+                customerAddViewModel.FirstNameVM = SelectedItem.FirstName;
+                customerAddViewModel.LastNameVM = SelectedItem.LastName;
+                customerAddViewModel.PhoneNumberVM = SelectedItem.PhoneNumber;
+                customerAddViewModel.EmailVM = SelectedItem.Email;
+                customerAddViewModel.LoyaltyCardIdVM = SelectedItem.LoyaltyCardId;
+
+                switch (SelectedItem.Gender)
+                {
+                    case "Male":
+                        customerAddViewModel.IsMaleCheckedVM = true;
+                        break;
+                    case "Female":
+                        customerAddViewModel.IsFemaleCheckedVM = true;
+                        break;
+                    case "Other":
+                        customerAddViewModel.IsOtherCheckedVM = true;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                CustomerFront customer = customerAddViewModel.GetCustomer();
+                Customers.Remove(SelectedItem);
+                Customers.Add(customer);
+
+                //
+                //
+                // ~~ Needs to be sorted. Do not forget. Moze Sale ~~
+                //
+                //
+
+                //SelectedItem = null; ~~ Doesn't work
+                CanAlter = false;
+                CanDelete = false;
+
+                CurrentCustomerViewModel = customerFilterViewModel;
+            }
+        }
+
+        private void OnSelect()
+        {
+            if(SelectedItem != null)
+            {
+                CanAlter = true;
+                CanDelete = true;
+            }
         }
 
         private void OnNav(string obj)
@@ -26,7 +143,13 @@ namespace ViewModel.ViewModels
             switch (obj)
             {
                 case "add":
-                    CurrentCustomerViewModel = customerAddViewModel;
+                    if(CurrentCustomerViewModel != customerAddViewModel)
+                        CurrentCustomerViewModel = customerAddViewModel;
+                    else
+                    {
+                        Customers.Add(customerAddViewModel.GetCustomer());
+                        OnNav("filter");
+                    }
                     break;
                 case "filter":
                     CurrentCustomerViewModel = customerFilterViewModel;
@@ -39,7 +162,6 @@ namespace ViewModel.ViewModels
                     break;
             }
         }
-
         public BindableBase CurrentCustomerViewModel
         {
             get { return currentCustomerViewModel; }
@@ -48,5 +170,53 @@ namespace ViewModel.ViewModels
                 SetProperty(ref currentCustomerViewModel, value);
             }
         }
+
+        public bool CanAlter
+        {
+            get { return canAlter; }
+            set
+            {
+                if (canAlter != value)
+                {
+                    canAlter = value;
+                    OnPropertyChanged("CanAlter");
+                }
+            }
+        }
+        public bool CanDelete
+        {
+            get { return canDelete; }
+            set
+            {
+                if (canDelete != value)
+                {
+                    canDelete = value;
+                    OnPropertyChanged("CanDelete");
+                }
+            }
+        }
+
+
+        public CustomerFront SelectedItem
+        {
+            get { return selectedItem; }
+            set
+            {
+                if (selectedItem != value)
+                {
+                    selectedItem = value;
+                    RaisePropertyChanged("SelectedItem");
+                }
+            }
+        }
+        private void RaisePropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
     }
 }
--- ViewModel/ViewModels/CustomerViewModels/CustomerAddViewModel.cs
+++ ViewModel/ViewModels/CustomerViewModels/CustomerAddViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Model.FrontendModel;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,156 @@ namespace ViewModel.ViewModels.CustomerViewModels
 {
     public class CustomerAddViewModel : BindableBase
     {
+        //private int customerIdVM;
+        private string firstNameVM;
+        private string lastNameVM;
+        private string phoneNumberVM;
+        private string emailVM;
+        //private string genderVM;
+        //private int pointsVM;
+        private string loyaltyCardIdVM;
+     
+        private bool isMaleCheckedVM;
+        private bool isFemaleCheckedVM;
+        private bool isOtherCheckedVM;
+
+        private int idCnt = 1;
+
         public CustomerAddViewModel()
         {
         }
+        private string GetGender()
+        {
+            if (IsMaleCheckedVM) return "Male";
+            else if (IsFemaleCheckedVM) return "Female";
+            else if (IsOtherCheckedVM) return "Other";
+            else return "Unspecified";
+        }
+        public CustomerFront GetCustomer()
+        {
+            string gender = GetGender();
+            CustomerFront customerToAdd = 
+                new CustomerFront(IdCnt++, FirstNameVM, LastNameVM, PhoneNumberVM, EmailVM, gender, 0, LoyaltyCardIdVM);
+
+            ClearInput();
+
+            return customerToAdd;
+        }
+
+        public void ClearInput()
+        {
+            FirstNameVM = "";
+            LastNameVM = "";
+            PhoneNumberVM = "";
+            EmailVM = "";
+            LoyaltyCardIdVM = "";
+            IsMaleCheckedVM = false;
+            IsFemaleCheckedVM = false;
+            IsOtherCheckedVM = false;
+        }
+
+        public string FirstNameVM
+        {
+            get { return firstNameVM; }
+            set
+            {
+                if (firstNameVM != value)
+                {
+                    firstNameVM = value;
+                    OnPropertyChanged("FirstNameVM");
+                }
+            }
+        }
+        public string LastNameVM
+        {
+            get { return lastNameVM; }
+            set
+            {
+                if (lastNameVM != value)
+                {
+                    lastNameVM = value;
+                    OnPropertyChanged("LastNameVM");
+                }
+            }
+        }
+        public string PhoneNumberVM
+        {
+            get { return phoneNumberVM; }
+            set
+            {
+                if (phoneNumberVM != value)
+                {
+                    phoneNumberVM = value;
+                    OnPropertyChanged("PhoneNumberVM");
+                }
+            }
+        }
+
+        public string EmailVM
+        {
+            get { return emailVM; }
+            set
+            {
+                if (emailVM != value)
+                {
+                    emailVM = value;
+                    OnPropertyChanged("EmailVM");
+                }
+            }
+
+        }
+
+        public string LoyaltyCardIdVM
+        {
+            get { return loyaltyCardIdVM; }
+            set
+            {
+                if (loyaltyCardIdVM != value)
+                {
+                    loyaltyCardIdVM = value;
+                    OnPropertyChanged("LoyaltyCardIdVM");
+                }
+            }
+
+        }
+
+        public bool IsMaleCheckedVM
+        {
+            get { return isMaleCheckedVM; }
+            set
+            {
+                if(isMaleCheckedVM != value)
+                {
+                    isMaleCheckedVM = value;
+                    OnPropertyChanged("IsMaleCheckedVM");
+                }
+            }
+        }
+        public bool IsFemaleCheckedVM
+        {
+            get { return isFemaleCheckedVM; }
+            set
+            {
+                if (isFemaleCheckedVM != value)
+                {
+                    isFemaleCheckedVM = value;
+                    OnPropertyChanged("IsFemaleCheckedVM");
+                }
+            }
+        }
+        public bool IsOtherCheckedVM
+        {
+            get { return isOtherCheckedVM; }
+            set
+            {
+                if (isOtherCheckedVM != value)
+                {
+                    isOtherCheckedVM = value;
+                    OnPropertyChanged("IsOtherCheckedVM");
+                }
+            }
+        }
+
+        public int IdCnt { get => idCnt; set => idCnt = value; }
     }
 }
