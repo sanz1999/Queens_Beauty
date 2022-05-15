0011c5b84502e445eca21ca480b6ef627671c791
Stefan Scekic <stafanscekic023@gmail.com>
2022-05-15 19:55:54 +0200

    Sva AAAAAAAAA

--- Model/DBModel/DBSIA.cs
+++ Model/DBModel/DBSIA.cs
@@ -12,6 +12,8 @@ namespace Model.DBModel
         //1. polje je aid, 2. je sid
         public Tuple<int,int> id { get; set; }
         public int workerId { get; set; }
+        public double value { get; set; }
+        public string method { get; set; }
 
         public DBSIA(Tuple<int, int> id, int workerId)
         {
@@ -23,16 +25,24 @@ namespace Model.DBModel
         {
         }
 
+        public DBSIA(Tuple<int, int> id, int workerId, double value, string method)
+        {
+            this.id = id;
+            this.workerId = workerId;
+            this.value = value;
+            this.method = method;
+        }
+
         public static string GetHeader()
         {
-            return string.Format("\n{0, -12} {1, -12} {2, -12}",
-                "A_ID", "S_ID", "W_ID");
+            return string.Format("\n{0, -12} {1, -12} {2, -12} {3, -12} {4, -12}",
+                "A_ID", "S_ID", "W_ID", "PAID", "METHOD");
         }
 
         public override string ToString()
         {
-            return string.Format("{0, -12} {1, -12} {2, -12}",
-                id.Item1, id.Item2, workerId);
+            return string.Format("{0, -12} {1, -12} {2, -12} {3, -12} {4, -12}",
+                id.Item1, id.Item2, workerId, value, method);
         }
     }
 }
