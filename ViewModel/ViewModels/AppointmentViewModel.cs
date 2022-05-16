fe791d246c457ad00cb38f853f660d62d0cf1d1f
urospetrov <91796126+urospetrov@users.noreply.github.com>
2022-05-16 19:27:39 +0200

    Gas

--- ViewModel/ViewModels/AppointmentViewModel.cs
+++ ViewModel/ViewModels/AppointmentViewModel.cs
@@ -96,10 +96,17 @@ namespace ViewModel.ViewModels
                     AppointmentsSearch.RemoveAt(indexSearch);
                     AppointmentsSearch.Insert(indexSearch, newOne);
 
+                    SelectedItem = AppointmentsSearch[0];
+
                     CanAlter = false;
                     CanDelete = false;
 
                     CurrentAppointmentViewModel = appointmentFilterViewModel;
+
+                    //DO NOT TOUCH UNDER ANY CIRCUMSTANCES
+                    OnCancel();
+                    OnCancel();
+                    //DO NOT TOUCH UNDER ANY CIRCUMSTANCES
 
                     IsPayButtonVisible = "Collapsed";
                 }
@@ -266,13 +273,11 @@ namespace ViewModel.ViewModels
             if (SelectedItem == null)
                 return;
 
-
             IsPayButtonVisible = "Visible";
             if (SelectedItem.State)
                 CanPay = false;
             else
                 CanPay = true;
-
 
             CanAdd = true;
 
@@ -324,8 +329,9 @@ namespace ViewModel.ViewModels
                         {
                             AppointmentFront newAppointment = appointmentAddViewModel.GetAppointment();
                             appointmentCRUD.AddToDataBase(newAppointment);
-                            Appointments.Add(appointmentCRUD.FindLastAdded());
-                            AppointmentsSearch.Add(appointmentCRUD.FindLastAdded());
+                            newAppointment = appointmentCRUD.FindLastAdded();
+                            Appointments.Add(newAppointment);
+                            AppointmentsSearch.Add(newAppointment);
                             OnNav("filter");
 
                             CanAlter = false;
